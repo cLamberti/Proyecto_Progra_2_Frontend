edc6016831476f62970b4295a9ea79de4ab331c2
Chris Lamberti <lamberti.christopher@gmail.com>
2025-05-29 16:17:11 -0600

    Last-Changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'blog-spa' title`, () => {
+  it(`should have the 'coco-tours' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog-spa');
+    expect(app.title).toEqual('coco-tours');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blog-spa');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coco-tours');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import {CategoryService} from "./services/category.service"
 import { UserService } from './services/user.service';
 
 @Component({
@@ -10,35 +9,16 @@ import { UserService } from './services/user.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog-spa';
-  public categories:any;
-  private checkCategories;
+  title = 'coco-tours';
   private checkIdentity;
   public identity:any;
 
   constructor(
-    private categoryService:CategoryService,
-    private userService:UserService
+    private userService:UserService,
   
   ){
-    this.loadCategories()
-    this.checkCategories=setInterval(()=>{
-        this.loadCategories()
-    },5000)
     this.checkIdentity=setInterval(()=>{
       this.identity=userService.getIdentity()
     },500)
-  }
-  public loadCategories(){
-    this.categoryService.getCategories().subscribe({
-      next:(response:any)=>{
-        console.log(response)
-        this.categories=response
-      },
-      error:(err:Error)=>{
-        console.log(err)
-        this.categories=null
-      }
-    })
   }
 }
