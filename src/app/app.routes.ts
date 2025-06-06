5614b07d32476ec972bccfa040d247a13ba483c2
Chris Lamberti <lamberti.christopher@gmail.com>
2025-06-06 17:35:11 -0600

    fix app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,21 +3,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterClientComponent } from './components/register/register-client/register-client.component';
 import { ErrorComponent } from './components/error/error.component';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
-import { AdViajeComponent } from './components/ad-viaje/ad-viaje.component';
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'registro-client', component: RegisterClientComponent },
-  { path: 'registro-user', component: RegisterUserComponent },
-  { path: 'ad-viaje', component: AdViajeComponent },
-]
 import { RegisterUserClientComponent } from './components/register/register-user-client/register-user-client.component';
 import { RegisterAdminComponent } from './components/register/register-admin/register-admin.component';
 import { RegisterUserAdminComponent } from './components/register/register-user-admin/register-user-admin.component';
 import { UserComponent } from './components/gestionUsuarios/user/user.component';
 import { UsersComponent } from './components/gestionUsuarios/users/users.component';
+import { AdViajeComponent } from './components/ad-viaje/ad-viaje.component';
 import { User } from './models/user';
+
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'login',component:LoginComponent},
@@ -27,15 +20,8 @@ export const routes: Routes = [
     {path:'register-user-admin', component:RegisterUserAdminComponent},
     {path:'users', component:UsersComponent},
     {path:'user', component:UserComponent},
+    {path: 'ad-viaje', component: AdViajeComponent },
+    
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register-client', component: RegisterClientComponent },
-  { path: 'register-user-client', component: RegisterUserClientComponent },
-  { path: 'register-admin', component: RegisterAdminComponent },
-  { path: 'register-user-admin', component: RegisterUserAdminComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'user', component: UserComponent },
-  { path: '**', component: ErrorComponent }
+    {path:'**',component:ErrorComponent}
 ];
