8e8362bb121381768917331d46ed5532f9dc3313
Christopher Lamberti Chavarria <lamberti.christopher@gmail.com>
2025-06-06 16:57:27 -0600

    Revert "Fix some things for the alerts and made the users component and user …"

--- src/app/components/register/register-client/register-client.component.ts
+++ src/app/components/register/register-client/register-client.component.ts
@@ -8,7 +8,6 @@ import { timer } from 'rxjs';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import Swal from 'sweetalert2';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-client',
@@ -21,8 +20,7 @@ export class RegisterClientComponent {
 
   constructor(
     private clientService:ClientService,
-    public statusService:StatusService,
-    private router:Router
+    public statusService:StatusService
   ){
     this.statusService.status=-1
     this.client=new Client(0, "")
@@ -43,16 +41,7 @@ export class RegisterClientComponent {
           this.changeStatus(0)
           this.client.idClient = response.generated_id
           sessionStorage.setItem('identityClient', JSON.stringify(this.client.idClient));
-          Swal.fire({
-            title: 'exito',
-            text: 'Registro de nombre correcto, ya puede seguir con el registro',
-            icon:'success',
-            confirmButtonText:'Continuar?'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.router.navigate(['/register-user-client']);
-            }
-          });
+          Swal.fire('Éxito', 'Nombre guardado correctamente, dele a siguiente para continuar con el registro', 'success');
         }else{
           form.reset()
           this.changeStatus(1)
