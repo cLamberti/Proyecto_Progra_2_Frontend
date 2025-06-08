23e55d30a9555ae56dbfd8fee2fc16852ed46fd0
Pinaruma <juan98xxxx98@gmail.com>
2025-06-08 16:17:21 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterClientComponent } from './components/register/register-client/register-client.component';
 import { ErrorComponent } from './components/error/error.component';
-
-import { NewTravelDetailComponent } from './components/travel-detail/new-travel-detail/new-travel-detail.component';
-import { ListTravelDetailsComponent } from './components/travel-detail/list-travel-details/list-travel-details.component';
-import { SearchTravelDetailComponent } from './components/travel-detail/search-travel-detail/search-travel-detail.component';
-import { UpdateTravelDetailComponent } from './components/travel-detail/update-travel-detail/update-travel-detail.component';
-import { DeleteTravelDetailComponent } from './components/travel-detail/delete-travel-detail/delete-travel-detail.component';
 import { RegisterUserClientComponent } from './components/register/register-user-client/register-user-client.component';
 import { RegisterAdminComponent } from './components/register/register-admin/register-admin.component';
 import { RegisterUserAdminComponent } from './components/register/register-user-admin/register-user-admin.component';
@@ -16,7 +10,6 @@ import { UserComponent } from './components/gestionUsuarios/user/user.component'
 import { UsersComponent } from './components/gestionUsuarios/users/users.component';
 import { AdViajeComponent } from './components/ad-viaje/ad-viaje.component';
 import { User } from './models/user';
-import { AdProveedoresComponent } from './components/ad-proveedores/ad-proveedores.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
@@ -25,18 +18,9 @@ export const routes: Routes = [
     {path:'register-user-client', component:RegisterUserClientComponent},
     {path:'register-admin', component:RegisterAdminComponent},
     {path:'register-user-admin', component:RegisterUserAdminComponent},
-
-
-    {path:'crear-detalleViaje',component:NewTravelDetailComponent},
-    {path:'listar-detalleViaje',component:ListTravelDetailsComponent},    
-    {path:'buscar-detalleViaje',component:SearchTravelDetailComponent},
-    {path:'actualizar-detalleViaje',component:UpdateTravelDetailComponent},
-    {path:'borrar-detalleViaje',component:DeleteTravelDetailComponent},    
-
     {path:'users', component:UsersComponent},
     {path:'user', component:UserComponent},
-    {path: 'ad-viaje', component: AdViajeComponent },
-    {path: 'ad-proveedores', component: AdProveedoresComponent },    
-
+    {path: 'ad-viaje', component: AdViajeComponent },   
+    
     {path:'**',component:ErrorComponent}
 ];
