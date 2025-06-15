481c3999777b97741a8c06f73955e2b62363e703
Chris Lamberti <lamberti.christopher@gmail.com>
2025-06-14 18:05:59 -0600

    Update reservation.service.ts

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,5 +1,6 @@
+// src/app/services/reserva.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Reservation } from '../models/reservation';
 import { server } from './global';
@@ -7,48 +8,33 @@ import { server } from './global';
 @Injectable({
   providedIn: 'root'
 })
-export class ReservationService {
-
+export class ReservaService {
   private url: string;
 
   constructor(private http: HttpClient) {
     this.url = server.url;
   }
-
-  createReservation(token: string, reservation: Reservation): Observable<any> {
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('Authorization', token);
-
-    const body = JSON.stringify({
-      idclient: reservation.idCliente,
-      idadministrator: reservation.idAdministrador,
-      idDetail: reservation.idDetalle
-    });
-
-    return this.http.post(`${this.url}reservation`, body, { headers });
-  }
-  private getHeaders(): HttpHeaders {
-    const token = sessionStorage.getItem('token');
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token}` : ''
-    });
-  }
-   getReservationsByClient(idCliente: number): Observable<Reservation[]> {
-    return this.http.get<Reservation[]>(`${this.url}Reservations/${idCliente}`, {
-      headers: this.getHeaders()
-    });
-  }
-  deleteReservation(id: number): Observable<any> {
-    return this.http.delete(`${this.url}Reservations/delete/${id}`, {
-      headers: this.getHeaders()
-    });
+  getAllReservas(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(`${this.url}Reservations/all`);
   }
 
-  updateReservation(id: number, reservation: Reservation): Observable<any> {
-    return this.http.put(`${this.url}/Reservations/update/${id}`, reservation, {
-      headers: this.getHeaders()
-    }); 
+  getReservaById(id: number): Observable<Reservation> {
+    return this.http.get<Reservation>(`${this.url}Reservations/${id}`);
+  }
+
+  createReserva(reserva: { idUsuario: number; idDetail: number; estado: string }): Observable<Reservation> {
+    return this.http.post<Reservation>(`${this.url}Reservations`, reserva);
+  }
+
+  updateReserva(id: number, data: { idusuario: number; idDetail: number }): Observable<any> {
+    return this.http.put(`${this.url}Reservations/update/${id}`, data);
+  }
+
+  updateEstado(id: number, estado: string): Observable<any> {
+    return this.http.put(`${this.url}Reservations/status/${id}`, { estado });
+  }
+
+  deleteReserva(id: number): Observable<any> {
+    return this.http.delete(`${this.url}Reservations/delete/${id}`);
   }
 }
